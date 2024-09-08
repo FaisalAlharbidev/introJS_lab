29d8f8b5ec30f929192545f0fc1bae4e7e7ccdb9
Faisal Fahad <faisal.dev49@gmail.com>
2024-09-08 12:58:42 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+let x =400;
+
+let y = 400;
+
+
+console.log("The area of the rectangle is : ", x*y)
+
+
+let c = 44
+let f = (c*1.8)+32
+console.log("الدرجة المحولة من مئوية 44 الى فينهراية " , f)
+
+
+
+let sec=1*60*60
+
+console.log("1 hours = " , sec , "  Seconds"  )
+
+
+let thingOne="بسم "
+
+let thingTwo = "الله"
+console.log(thingOne+ " " ,thingTwo )
+
+
+
+let bail = 10.25 + 3.99 + 7.15 
+
+let tips = bail * 0.15 
+
+let totil = bail+tips
+console.log("the totil is : "  , totil)
+
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+
+
+let maidlab="The Intro to JavaScript course is "+adjective1+" James and Julia are so" +adjective2+"I cannot wait to work through the rest of this "+adjective3+"content!";
+
+console.log(maidlab)
